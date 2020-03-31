ab6679a7ecb591fcc2fc8d2f0e30bec5b0f5e9d7
yihelei <577395998@qq.com>
2020-03-31 09:52:46 +0800

    “提交商品订单实体类、网页、login controller

---
+++ kuajingdianshangpingtai/src/main/java/com/neu/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.neu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/log")
+	public String log(){
+		return "login";
+	}
+	
+}
