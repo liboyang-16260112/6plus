b580266827d8faa99bd95aa7962373066b1d7c21
yihelei <577395998@qq.com>
2020-03-31 18:14:41 +0800

    提交usercon

---
+++ kuajingdianshangpingtai/src/main/java/com/neu/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.neu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.neu.service.UserService;
+
+
+
+@Controller
+public class UserController {
+
+	@Autowired
+	private   UserService userService;
+	 @RequestMapping("/regist")
+	    public  String  regist(){
+	    	return "reg";
+	    }
+	    @RequestMapping("/register")
+	    public  String   register(String username,String pwd,String identity){
+	    	return  userService.register(username, pwd,identity);
+	    }
+	    @RequestMapping("/userlog")
+	    public   String  userlog(String username,String pwd,String identity){
+	    	return  userService.login(username, pwd, identity);
+	    }
+	    
+	    @RequestMapping("/log")
+	    public  String   log(){
+	    	return  "login";
+	    }
+}
